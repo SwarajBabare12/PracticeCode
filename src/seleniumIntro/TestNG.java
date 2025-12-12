f83c7b484556df7f3c9ddf90b745ab29c92fd891
Swaraj <swarajbabare2@gmail.com>
2025-12-12 10:38:38 +0530

    this changes done by Me

--- src/seleniumIntro/TestNG.java
+++ src/seleniumIntro/TestNG.java
@@ -8,6 +8,12 @@ public class TestNG {
 	public void demo1() {
 		
 		System.out.println("Hello TestNG");
+		System.out.println("Hello TestNG");
+		System.out.println("Hello TestNG");
+		System.out.println("Hello TestNG");
+		System.out.println("Hello TestNG");
+		System.out.println("Hello TestNG");
+		System.out.println("Hello TestNG");System.out.println("Hello TestNG");
 		
 	}
 	
