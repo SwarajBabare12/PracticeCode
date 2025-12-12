003ec56fa92397c9216b4e89a1aaa36f86cbd67e
Swaraj <swarajbabare2@gmail.com>
2025-12-12 12:27:00 +0530

    Changes done by admin

--- src/com/java/conceptPractice/GitDEMO.java
+++ src/com/java/conceptPractice/GitDEMO.java
@@ -1,5 +1,22 @@
 package com.java.conceptPractice;
 
-public class GitDEMO {
+import org.testng.annotations.Test;
 
-}
+public class GitDEMO {
+	
+	@Test
+	public class Git{
+		
+		public void demo1() {
+			
+			System.out.println("Hello TestNG");
+			System.out.println("Hello TestNG");
+			System.out.println("Hello TestNG");
+			System.out.println("Hello TestNG");
+			System.out.println("Hello TestNG");
+			System.out.println("Hello TestNG");
+			System.out.println("Hello TestNG");System.out.println("Hello TestNG");
+			
+		}
+
+	}}
